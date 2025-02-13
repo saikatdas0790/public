54e3e44380cff5b865fa4ed8f7d9f755bdcfe676
Saikat Das <saikatdas0790@gmail.com>
2025-02-13 12:26:12 +0530

    chore: set right path

--- device-initial-setup/windows/readme.sh
+++ device-initial-setup/windows/readme.sh
@@ -1,5 +1,5 @@
 # Invoke Github hosted powershell script
 
 ```powershell
-Invoke-Expression "& { $(Invoke-RestMethod https://raw.githubusercontent.com/saikatdas0790/.dotfiles/refs/heads/main/windows/1-bootstrap.ps1) }"
+Invoke-Expression "& { $(Invoke-RestMethod https://raw.githubusercontent.com/saikatdas0790/public/refs/heads/main/device-initial-setup/windows/1-bootstrap.ps1) }"
 ```
