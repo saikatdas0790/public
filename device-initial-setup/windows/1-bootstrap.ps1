c5df8f0669a9e7be1d643b2b05ea49d314de0d1e
Saikat Das <saikatdas0790@gmail.com>
2025-02-13 12:39:01 +0530

    chore: install github cli

--- device-initial-setup/windows/1-bootstrap.ps1
+++ device-initial-setup/windows/1-bootstrap.ps1
@@ -1,1 +1,13 @@
-Get-WindowsUpdate
+# # Install the Windows Update module
+# Install-Module -Name PSWindowsUpdate -Force
+
+# # Import the Windows Update module
+# Import-Module PSWindowsUpdate
+
+# # Check for updates
+# Get-WindowsUpdate -AcceptAll -Install -AutoReboot
+
+# # Restart the system if updates require a reboot
+# Restart-Computer -Force
+
+winget install GitHub.cli
